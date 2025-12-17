60ebdd37ab49cb3ef8ba2a46eea311a6ce84bbe2
rutaab-ahmed <rutaab.pfepl@gmail.com>
2025-12-17 17:52:38 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -436,17 +436,16 @@ app.put("/api/record/:id", async (req, res) => {
   }
 });
 
-
-
-
-
 /* ================= SERVER ================= */
 
 // app.listen(8083, () =>
 //   console.log("🚀 Server running on http://localhost:8083")
 // );
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+const PORT = process.env.PORT || 8083;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
 
