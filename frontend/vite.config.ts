bb0c8905081565b4c62b45a78474a8173a01a841
rutaab-ahmed <rutaab.pfepl@gmail.com>
2025-12-17 17:06:49 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
         allowedHosts:["sleepier-undeparted-audrie.ngrok-free.dev"],
       },
       plugins: [react()],
-        build: {outDir: 'dist',}
+      build: {outDir: 'dist'},
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
